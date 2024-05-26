0a5728d5d425cad341af426080d34b6d5b1877a7
Nicolas Bernal <nicolasbernal105@gmail.com>
2024-05-26 15:27:28 -0500

    Actualizacion Models API

--- ganado/ganado.model.js
+++ ganado/ganado.model.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const schemaProducto = new mongoose.Schema({
-    userID: {type: String, required: true},
-    razaID: {type: String, required: true},
-    fechaNac: {type: Date, required: true},
-    genero: {type: String, required: true},
-    isActive: { type: Boolean, required: true, default: true } // Soft Delete
-  }, {
-    versionKey: false,
-    timestamps: true
+  userID: { type: String, required: true },
+  raza: { type: String, required: true }, // Se cambia para que tome el nombre, no el ID. 
+  fechaNac: { type: Date, required: true },
+  genero: { type: String, required: true },
+  isActive: { type: Boolean, required: true, default: true } // Soft Delete 
+}, {
+  versionKey: false,
+  timestamps: true
 });
-  
+
 const Ganado = mongoose.model('Ganado', schemaProducto);
 
 module.exports = Ganado;
--- raza/raza.model.js
+++ raza/raza.model.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const schemaProducto = new mongoose.Schema({
-    userID: {type: String, required: true},
-    nombre: {type: String, required: true},
-    // Valores de salud por tipo de raza a determinar...
-    isActive: { type: Boolean, required: true, default: true } // Soft Delete
-  }, {
-    versionKey: false,
-    timestamps: true
+  userID: { type: String, required: true },
+  nombreRaza: { type: String, required: true },
+  pesoMinimo: { type: Number, required: true },
+  pesoMaximo: { type: Number, required: true },
+  temperaturaCorporal: { type: Number, required: true },
+  estaturaMinima: { type: Number, required: true },
+  estaturaMaxima: { type: Number, required: true },
+  genero: { type: String, required: true },
+  isActive: { type: Boolean, required: true, default: true } // Soft Delete 
+}, {
+  versionKey: false,
+  timestamps: true
 });
-  
+
 const Raza = mongoose.model('Raza', schemaProducto);
 
 module.exports = Raza;
