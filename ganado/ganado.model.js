f13c9b7bfc9b94b5f6b3781dd7db433ae1e450cc
JuanCSaieh1 <juanca_saieh@hotmail.com>
2024-05-29 01:04:13 -0500

    fixed genero

--- ganado/ganado.model.js
+++ ganado/ganado.model.js
@@ -4,7 +4,6 @@ const schemaProducto = new mongoose.Schema({
   userID: { type: String, required: true },
   raza: { type: String, required: true }, // Se cambia para que tome el nombre, no el ID. 
   fechaNac: { type: Date, required: true },
-  genero: { type: String, required: true },
   isActive: { type: Boolean, required: true, default: true } // Soft Delete 
 }, {
   versionKey: false,
